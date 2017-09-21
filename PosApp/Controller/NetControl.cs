213d9046e3f2c0178d1a01ed24c48854c1a47eb1
993902025 <993902025@qq.com>
2017-09-21 17:55:42 +0800

    chonggou

---
+++ PosApp/Controller/NetControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotPos.Controller
+{
+    public class NetControl
+    {
+        private string dataLength, dataType, dataOrd, dataOverSign, handleOrd;
+
+        private string dataBody;
+
+        private string mac;
+
+        private string numSign;
+
+        Socket serverSocket;
+
+        public NetControl() { }
+
+        public NetControl(string dataType, string dataBody )
+        {
+            this.dataType = dataType;
+            this.dataBody = dataBody;
+        }
+
+        public void IniNet()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Connect(string ip, int port)
+        {
+            serverSocket.Connect(ip, port);
+        }
+
+
+    }
+}
